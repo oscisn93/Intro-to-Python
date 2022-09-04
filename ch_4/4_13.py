617052f3abfe9e40435dbbdf56c757ebffb127b5
oscisn93 <oscar.cisneros@thelastmile.org>
2022-09-04 15:49:53 -0700

    4.13

---
+++ ch_4/4_13.py
@@ -0,0 +1,10 @@
+print('original tuple: ')
+foods  = ('chicken soup','beef stew','tacos','ceviche','pancakes')
+for food in foods:
+    print(food)
+
+print('\nmodified tuple: ')
+foods  = ('soup','chiken stew','tacos','ceviche','pancakes')
+for food in foods:
+    print(food)
+    
