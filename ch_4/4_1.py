8eb401bcbb0fe6c2ae5edcb50d9404352981339a
oscisn93 <oscar.cisneros@thelastmile.org>
2022-09-04 14:23:23 -0700

    Exercise 4.1 - Assignment 3

---
+++ ch_4/4_1.py
@@ -0,0 +1,4 @@
+pizzas = ['pepperoni', 'meat-lovers', 'cheese']
+for pizza in pizzas:
+    print('I like ', pizza, ' pizza.')
+print('Pizza is amazing, too bad you can't eat it every day!')
--- util/addfiles.py
+++ util/addfiles.py
@@ -2,10 +2,15 @@
 # the file programmatically and run program
 # from util folder
 
+import os
+# import sys
+
 ASSIGNMENT_NUMS = [8,9,10,11,12]
-CHAPTER_PREFIX = '5_'
+CHAPTER = '5'
+
+path_to_files = f'{os.path}../ch_'
 
 for num in ASSIGNMENT_NUMS:
-    filename = f'{CHAPTER_PREFIX}{num}.py'
-    file = open(filename, 'x')
-    file.close()
+   filename = f'{path_to_files}{CHAPTER}/{CHAPTER}_{num}.py'
+   file = open(filename, 'x')
+   file.close()
