8095a5ae01b5ce72bcf2f6e856200d197e36afe9
oscisn93 <oscar.cisneros@thelastmile.org>
2022-09-04 14:47:03 -0700

    4.3

--- ch_4/4_1.py
+++ ch_4/4_1.py
@@ -1,4 +1,4 @@
 pizzas = ['pepperoni', 'meat-lovers', 'cheese']
 for pizza in pizzas:
     print('I like ', pizza, ' pizza.')
-print('Pizza is amazing, too bad you can't eat it every day!')
+print('Pizza is amazing, too bad you can\'t eat it every day!')
---
+++ ch_4/4_3.py
@@ -0,0 +1,3 @@
+UP_TO = 20
+for i in range(0, UP_TO):
+    print(i+1)
