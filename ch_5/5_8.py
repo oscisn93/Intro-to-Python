04ff35dac1f18688cb0027df068f8920057499af
oscisn93 <oscar.cisneros@thelastmile.org>
2022-09-07 13:51:21 -0700

    5.8

---
+++ ch_5/5_8.py
@@ -0,0 +1,6 @@
+usernames = ['admin', 'Jaden', 'Matt', 'Judy', 'Alex']
+for username in usernames:
+    if username == 'admin':
+        print('Hello ',username,'\nWould you like a status report?')
+    else:
+        print('Hello ',username,'\nNo status report for you normie!')
