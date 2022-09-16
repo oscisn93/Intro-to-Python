cda8a838b7203e8240517385cf1673d760a59e46
oscisn93 <oscar.cisneros@thelastmile.org>
2022-09-15 22:14:07 -0700

    5.12

--- addfiles.py
+++ addfiles.py
@@ -2,15 +2,11 @@
 # the file programmatically and run program
 # from util folder
 
-import os
-# import sys
+ASSIGNMENT_NUMS = [1,2,3,5,6,7,10,11]
+CHAPTER = '6'
 
-ASSIGNMENT_NUMS = [8,9,10,11,12]
-CHAPTER = '5'
-
-path_to_files = f'{os.path}../ch_'
 
 for num in ASSIGNMENT_NUMS:
-   filename = f'{path_to_files}{CHAPTER}/{CHAPTER}_{num}.py'
+   filename = f'{CHAPTER}_{num}.py'
    file = open(filename, 'x')
    file.close()
--- ch_5/5_12.py
+++ ch_5/5_12.py
@@ -1,5 +1,6 @@
 cities = ['Riverside', 'Corona', 'Anaheim']
 age_cohorts = ['18-20', '21-25', '26+']
-composite = [f'{city} {age}' for city in cities for age in age_cohorts ]
+composite = [f'{city} {age}' for city in cities for age in age_cohorts]
+
 for comp in composite:
     print(comp)
