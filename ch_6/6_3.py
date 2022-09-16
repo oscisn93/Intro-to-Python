3484c72716608b1c0a34c5a80f66eeab695d7be0
oscisn93 <oscar.cisneros@thelastmile.org>
2022-09-15 22:14:59 -0700

    6.3

---
+++ ch_6/6_3.py
@@ -0,0 +1,10 @@
+glossary = {
+    'List': 'A dynamic python array.',
+    'Dictionary': 'A python data structure that stores key value pairs.',
+    'Linux': 'An open source operating system kernel that was originally a unix clone.',
+    'Windows': 'A proprietary operating system famous for the infamnous \"Blue Screen of Death\".',
+    'Tuple': 'A python data type that is much like a list except it is immutable'
+}
+
+for word, definition in glossary.items():
+    print(f'{word} :\n{definition}\n')
