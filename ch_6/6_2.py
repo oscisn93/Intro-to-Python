0dca8a40aeaea29796fd2d31fb63223c122be1ff
oscisn93 <oscar.cisneros@thelastmile.org>
2022-09-15 22:14:48 -0700

    6.2

---
+++ ch_6/6_2.py
@@ -0,0 +1,10 @@
+fav_nums = {
+    'Noemi': 12,
+    'Katy': 22,
+    'Brandon': 50,
+    'Haley': 7,
+    'Brenda': 13
+}
+
+for person, num in fav_nums.items():
+    print(f'{person}\'s favorite number is {num}')
