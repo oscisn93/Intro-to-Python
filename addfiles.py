01a38a6fc8aa0581f7126481afbddbf016948396
oscisn93 <oscar.cisneros@thelastmile.org>
2022-09-26 16:56:18 -0700

    created files for ch 7 exercises

--- addfiles.py
+++ addfiles.py
@@ -2,8 +2,8 @@
 # the file programmatically and run program
 # from util folder
 
-ASSIGNMENT_NUMS = [1,2,3,5,6,7,10,11]
-CHAPTER = '6'
+ASSIGNMENT_NUMS = [1,3,4,5,8,9,10,11,12,13,14]
+CHAPTER = '7'
 
 
 for num in ASSIGNMENT_NUMS:
---
+++ ch_7/7_1.py
@@ -0,0 +1,2 @@
+car = input("What kind of car would you like? ")
+print(f"Let's see if I can find you a {car}...")
