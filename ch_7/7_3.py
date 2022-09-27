7d2022212706e368902efa8ec12d6ee19d5d51c6
oscisn93 <oscar.cisneros@thelastmile.org>
2022-09-26 17:03:00 -0700

    7.3

---
+++ ch_7/7_3.py
@@ -0,0 +1,6 @@
+number = input("Give me a number and I'll tell you if it's a multiple of ten: ")
+if int(number)%10 == 0:
+    print("yup that's a multiple of ten!")
+else:
+    print("That's not a multiple of ten!")
+    
