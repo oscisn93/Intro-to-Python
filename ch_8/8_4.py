68a9f4abadb054099088a68c078ae43e6a31e01c
oscisn93 <oscar.cisneros@thelastmile.org>
2022-10-03 16:40:29 -0700

    8.4

---
+++ ch_8/8_4.py
@@ -0,0 +1,5 @@
+def make_shirt(size="M", text="I love Python"):
+    print(f"A size {size} shirt will be made with the symbols: {text}.\n")
+make_shirt()
+make_shirt(size="L")
+make_shirt("XL", "I love C")
