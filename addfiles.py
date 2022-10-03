199ccbd6675fa327c3f452ffa89a6c6ec7014b43
oscisn93 <oscar.cisneros@thelastmile.org>
2022-10-03 16:39:50 -0700

    8.1

--- addfiles.py
+++ addfiles.py
@@ -2,8 +2,8 @@
 # the file programmatically and run program
 # from util folder
 
-ASSIGNMENT_NUMS = [1,3,4,5,8,9,10,11,12,13,14]
-CHAPTER = '7'
+ASSIGNMENT_NUMS = [1,2,3,4,6,7,9,10,11,12,13,14,15]
+CHAPTER = '9'
 
 
 for num in ASSIGNMENT_NUMS:
---
+++ ch_8/8_1.py
@@ -0,0 +1,5 @@
+def display_message():
+    msg = "Im learning about functions and modules in Python!"
+    print(msg)
+
+display_message()
