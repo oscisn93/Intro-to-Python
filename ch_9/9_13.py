9c526e2580e73b4cc9b53b893f2ee5c47f367ea5
oscisn93 <oscar.cisneros@thelastmile.org>
2022-10-07 14:45:11 -0700

    9.13

--- ch_9/9_13.py
+++ ch_9/9_13.py
@@ -4,8 +4,21 @@ class Die:
     def __init__(self, sides=6):
         self.sides = sides
     def roll_die(self):
-        print('Result:',randint(1, self.sides))
+        print(self.sides,'sided dieroll result:',randint(1, self.sides))
 
 six_sided = Die()
+ten_sided = Die(10)
+twenty_sided = Die(20)
+
+print('Starting simulations...\n')
 for i in range(0, 10):
     six_sided.roll_die()
+print('')
+
+for i in range(0, 10):
+    ten_sided.roll_die()
+print('')
+
+for i in range(0, 10):
+    twenty_sided.roll_die()
+print('\nSimulation complete!')
