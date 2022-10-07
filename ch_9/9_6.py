de3cfd28ebe0afa20e4185dfda2cfbd2c2d12320
oscisn93 <oscar.cisneros@thelastmile.org>
2022-10-07 14:21:25 -0700

    9.6

---
+++ ch_9/9_6.py
@@ -0,0 +1,32 @@
+from Restaurant import Restaurant
+
+class iceCreamStand(Restaurant):
+    def __init__(self, name, ctype, flavors):
+        super().__init__(name, ctype)
+        self.flavors = flavors
+    
+    def print_flavors(self):
+        n = len(self.flavors)
+        switch = [
+            self.flavors[0],
+            f'{self.flavors[0]} and {self.flavors[1]}',
+        ]
+        if n < 3:
+            if  n < 1:
+                print('The',self.ctype,'Sorry the shop is all out of flavors')
+                return
+            flavor_str = switch.get(n)
+        else:
+            flavor_str = ''
+            index = 0
+            for flavor in self.flavors:
+                if index == (n - 1):
+                    flavor_str += f'and {flavor}'
+                else:
+                    flavor_str += f'{flavor}, '
+                index += 1
+        print('This shop has the flavors:',flavor_str)
+        
+flavors = ['chocolate', 'vanilla', 'sherbert']
+bobs = iceCreamStand('Bill\'s', 'ice cream', flavors)
+bobs.print_flavors()
