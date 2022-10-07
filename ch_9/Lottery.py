ec4a3841f64d24deacc60bc2db641fb52fe6e532
oscisn93 <oscar.cisneros@thelastmile.org>
2022-10-07 15:25:21 -0700

    9.14

--- ch_9/Lottery.py
+++ ch_9/Lottery.py
@@ -1,5 +1,7 @@
+# 9.14
 import string
 from random import randint
+
 class Lottery:
     
     def __init__(self,series = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D"]):
@@ -17,9 +19,12 @@ class Lottery:
              winning_ticket += self.letters[randint(0,(len(self.letters)-1))]
              n -= 1
         return winning_ticket
+    
+    def buy_ticket(self):
+        return self.get_winning_ticket_number()
 
-lott = Lottery()
-winning_ticket_list = [lott.get_winning_ticket_number() for i in range(0,10)]
+# lott = Lottery()
+# winning_ticket_list = [lott.get_winning_ticket_number() for i in range(0,10)]
 
-for winning_ticket in winning_ticket_list:
-    print('Winning ticket:',winning_ticket)
+# for winning_ticket in winning_ticket_list:
+#     print('Winning ticket:',winning_ticket)
