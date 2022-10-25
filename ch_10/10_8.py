99bdbdd4d879ce66b7723a220ad274f87ef9da12
Oscar <ocisneros1@csu.fullerton.edu>
2022-10-24 17:39:09 -0700

    current progress on chapter 10: Exceptions

--- ch_10/10_8.py
+++ ch_10/10_8.py
@@ -6,3 +6,15 @@
 # Temporarily rename each of your files, 
 # and make sure the code in the except block executes properly.  
 # Make sure an exception is caught for either file missing.
+
+def printFile(name):
+    try:
+        with open(name) as file:
+            print(f'\t{name} contents:')
+            contents = file.read()
+            print(contents)
+    except FileNotFoundError:
+        print(f'Sorry, but the file {name} does not exist.')
+
+printFile('cats.txt')
+printFile('dogs.txt')
