7352ac5b70e0d56af95d6c39f9004b9b72393ac2
Oscar Cisneros <41174627+oscisn93@users.noreply.github.com>
2022-11-08 11:06:16 -0800

    Update 10_13.py

--- ch_10.2/10_13.py
+++ ch_10.2/10_13.py
@@ -1,10 +1,3 @@
-# The final listing for remember_me.py assumes either that the user 
-# has already entered their username or that the program is running for the first time. 
-# We should modify it in case the current user is not the person who last used the program.
-
-# Before printing a welcome back message in greet_user(), 
-# ask the user if this is the correct username. 
-# If it’s not, call get_new_username() to get the correct username.
 import json
 
 def get_stored_username():
